c716a23a9e9c6b0673338f198bd4f5b5fd3a275f
Raul Ac <raulesac21@gmail.com>
2025-07-29 12:27:53 -0600

    agregar archivos del juego

---
+++ app.js
@@ -0,0 +1,59 @@
+let listaamigos = [];
+
+/*function limpiar() {
+    document.querySelector('#amigo').value = '';
+    let s = document.getElementById('amigosorteado');
+    s.innerHTML = '';
+}*/
+
+function limpiarlista(){
+    
+    let lista = document.getElementById('listaAmigos');
+    lista.innerHTML = '';
+}
+
+function agregarAmigo(){
+    let amigo = document.getElementById('amigo').value;
+
+     if (amigo === ''){
+        alert('Por favor, inserte un nombre.');        
+        return;
+    }
+
+     if (listaamigos.includes(amigo)){
+        alert('Ese Amigo Ya Lo Agregaste'); 
+        return;       
+
+    }
+        listaamigos.push(amigo); //agregar a la lista
+        console.log(listaamigos); // ver amigos en consola
+       // limpiar();
+        mostrarAmigos(); //mostrar lista
+    
+ }
+ function mostrarAmigos(){
+    
+    let lista = document.getElementById('listaAmigos');
+    lista.innerHTML = '';
+
+    listaamigos.forEach(function(amigos) {
+    lista.innerHTML += `<li>${amigos}</li>`;});
+}
+
+  function sortearAmigo(){
+    let resultado = document.getElementById('amigosorteado');
+
+    if (listaamigos.length === 0) {
+    resultado.innerHTML = 'No hay amigos para sortear.';
+    return;
+  }
+
+  let amigoAleatorio = Math.floor(Math.random() * listaamigos.length);
+  let amigoSorteado = listaamigos[amigoAleatorio];
+  limpiarlista(); //Quitar lista 
+
+  resultado.innerHTML = `🎉 El amigo sorteado es: <strong>${amigoSorteado}</strong>`;
+
+}
+
+ 
